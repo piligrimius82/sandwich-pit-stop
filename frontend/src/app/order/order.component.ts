d9e397b0c5ff03a7a587dcfb027ac72beb20b342
Ishmael Opong <iopongkd@gmail.com>
2017-07-30 16:37:40 -0400

    order page changes

--- frontend/src/app/order/order.component.ts
+++ frontend/src/app/order/order.component.ts
@@ -1,13 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import {Order} from '../entities/order';
+import { OrderService } from '../order-utils/order.service';
+
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrls: ['./order.component.css']
 })
-export class OrderComponent {
+export class OrderComponent implements OnInit {
 
-  constructor() { }
+  private orderArray: any[];
 
+
+  //An example file json array of an order
+  people: any[] = [
+    {
+      'orderId': '1',
+      'sandwich': [
+        {
+          "meat": "Chicken",
+          "cheese": "Cheddar",
+          "price": "$3.00"
+        }
+      ]
+    },
+  ];
+
+  constructor(private orderService: OrderService) { }
+
+  ngOnInit() {
+      this.orderService.getOpenOrders().subscribe(res=> this.orderArray = res);
+  }
 
 }
